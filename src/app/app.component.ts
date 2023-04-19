adb9fccb91f7bc60ced2c9f207bb1543b3758bca
Nelson Tontarelli <nelartz01@gmail.com>
2023-04-18 22:17:49 -0300

    First Commit Simple CRUD

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Employee } from './models/employee'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  employeeArray: Employee[] = [
+    { id: 1, name: "Nelson", country: "Argentina" },
+    { id: 2, name: "Dianela", country: "Argentina" }
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+
+  }
+
+  openEdit(employee: Employee) {
+
+    this.selectedEmployee = employee;
+
+  }
+
+  delete(){
+    this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+    this.selectedEmployee = new Employee();
+  }
+  
+
+
+
+
+}
